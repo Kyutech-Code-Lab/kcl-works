58b9781f5ebd68604cc6d4d781bdec07cc97bd84
nao317 <country.gentleman.0317@gmail.com>
2026-01-19 07:50:48 +0900

    CardList

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,9 +1,10 @@
 import styles from "./styles.module.css";
 import EventCard from "@/components/EventCard";
 import ProductCard from "@/components/ProductCard";
+import { Event, Product } from "@/components/lib/microcms";
 
 interface CardListProps {
-  contents: any[];
+  contents: (Event | Product)[];
   isEvent: boolean;
 }
 
@@ -12,19 +13,25 @@ const CardList = ({ contents, isEvent }: CardListProps) => {
     <ul className={styles["card-list"]}>
       {contents.map((content) =>
         isEvent ? (
-          <li key={content.title}>
+          <li key={content.id}>
             <EventCard
+              id={content.id}
               title={content.title}
-              date={content.date}
-              thumbnailUrl={content.thumbnailUrl}
+              date={(content as Event).date}
+              thumbnailUrl={(content as Event).thumbnail?.url || "/dummy.jpg"}
             />
           </li>
         ) : (
-          <li key={content.title}>
+          <li key={content.id}>
             <ProductCard
-              title={content.title}
-              thumbnailUrl={content.thumbnailUrl}
-              tags={content.tags}
+              id={content.id}
+              title={(content as Product).title}
+              thumbnailUrl={(content as Product).thumbnail?.url || "/dummy.jpg"}
+              tags={
+                (content as Product).tags
+                  ?.map((tag) => tag.name)
+                  .filter(Boolean) as string[] || []
+              }
             />
           </li>
         )
