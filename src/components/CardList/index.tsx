cf277c17c56b420f9dddcb69d53a50c115742ca1
U5 <kyutech.joko@gmail.com>
2026-01-30 21:41:48 +0900

    fix:asの多用回避#23

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,41 +1,48 @@
 import styles from "./styles.module.css";
 import EventCard from "@/components/EventCard";
 import ProductCard from "@/components/ProductCard";
-import { Event, Product } from "@/components/lib/microcms";
+import type { Event, Product } from "@/lib/microcms";
 
-interface CardListProps {
-  contents: (Event | Product)[];
-  isEvent: boolean;
+interface EventCardListProps {
+  contents: Event[];
+  isEvent: true;
 }
+
+interface ProductCardListProps {
+  contents: Product[];
+  isEvent: false;
+}
+
+type CardListProps = EventCardListProps | ProductCardListProps;
 
 const CardList = ({ contents, isEvent }: CardListProps) => {
   return (
     <ul className={styles["card-list"]}>
-      {contents.map((content) =>
-        isEvent ? (
-          <li key={content.id}>
-            <EventCard
-              id={content.id}
-              title={content.title}
-              date={(content as Event).date}
-              thumbnailUrl={(content as Event).thumbnail?.url || "/dummy.jpg"}
-            />
-          </li>
-        ) : (
-          <li key={content.id}>
-            <ProductCard
-              id={content.id}
-              title={(content as Product).title}
-              thumbnailUrl={(content as Product).thumbnail?.url || "/dummy.jpg"}
-              tags={
-                (content as Product).tags
-                  ?.map((tag) => tag.name)
-                  .filter(Boolean) as string[] || []
-              }
-            />
-          </li>
-        )
-      )}
+      {isEvent
+        ? contents.map((event) => (
+            <li key={event.id}>
+              <EventCard
+                id={event.id}
+                title={event.title}
+                date={event.date}
+                thumbnailUrl={event.thumbnail?.url || "/dummy.jpg"}
+              />
+            </li>
+          ))
+        : contents.map((product) => (
+            <li key={product.id}>
+              <ProductCard
+                id={product.id}
+                title={product.title}
+                thumbnailUrl={product.thumbnail?.url || "/dummy.jpg"}
+                tags={
+                  product.tags?.map((tag) => tag.name).filter(Boolean) as
+                    | string[]
+                    | []
+                }
+              />
+            </li>
+          ))}
     </ul>
   );
 };
