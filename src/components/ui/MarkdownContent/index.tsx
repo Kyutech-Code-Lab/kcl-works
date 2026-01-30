a0e5e37dbfeaa0d67539ff2c9aaab1ac56ae546c
U5 <kyutech.joko@gmail.com>
2026-01-31 01:19:05 +0900

    update:mdにコードハイライト追加#24

--- src/components/ui/MarkdownContent/index.tsx
+++ src/components/ui/MarkdownContent/index.tsx
@@ -2,6 +2,10 @@ import ReactMarkDown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { parser } from "rich-editor-to-markdown-parser";
 import styles from "./styles.module.css";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import type { HTMLAttributes } from "react";
+import type { ExtraProps } from "react-markdown";
 
 type MarkdownContentProps = {
   content: string;
@@ -10,7 +14,28 @@ type MarkdownContentProps = {
 export default function MarkdownContent({ content }: MarkdownContentProps) {
   return (
     <div className={styles.markdown}>
-      <ReactMarkDown remarkPlugins={[remarkGfm]}>
+      <ReactMarkDown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code(props: HTMLAttributes<HTMLElement> & ExtraProps) {
+            const { children, className, node, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              <SyntaxHighlighter
+                PreTag="code"
+                language={match[1]}
+                style={atomOneDark}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
         {parser(content)}
       </ReactMarkDown>
     </div>
